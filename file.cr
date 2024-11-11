e6249165c13c52a5787c1b9138cea04459f509a5
Phil Samuel <philipmayr@outlook.com>
2024-11-11 12:33:31 -0600

    Update file.cr

--- file.cr
+++ file.cr
@@ -1,5 +1,5 @@
 def build_twelve_tone_equal_tempered_chromatic_scale(base_pitch_frequency)
-    starting_pitch_frequency = base_pitch_frequency / (2 << 4)
+    starting_pitch_frequency = base_pitch_frequency / (1 << 5)
   
     chromatic_scale = {"" => 0.0}
   
@@ -12,20 +12,22 @@ def build_twelve_tone_equal_tempered_chromatic_scale(base_pitch_frequency)
   
     half_step_counter += 1
   
-    123.times do |i|
+    123.times do
         if half_step_counter == 3
             octave_counter += 1
         elsif half_step_counter == 12
             starting_pitch_frequency *= 2
             half_step_counter = 0
         end
+
+        note = notes[half_step_counter]
   
         note_frequency = starting_pitch_frequency * (2 ** (half_step_counter / 12))
   
-        chromatic_scale[notes[half_step_counter] + octave_counter.to_s] = note_frequency
+        chromatic_scale[note + octave_counter.to_s] = note_frequency
   
-        print notes[half_step_counter] + octave_counter.to_s
-        puts " - " + note_frequency.to_s
+        puts note + octave_counter.to_s + " - " + note_frequency.to_s
+        
         half_step_counter += 1
     end
  
